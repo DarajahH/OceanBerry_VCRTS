d61f4a03abc7fcdbeb0dc5ff09d3fc88fcafb9d2
Darajah <148594071+DarajahH@users.noreply.github.com>
2026-02-19 15:38:02 -0500

    Refactor PanelExample to extend JFrame and add fields
    Refactor PanelExample class to extend JFrame and add user input fields.
    
    Signed-off-by: Darajah <148594071+DarajahH@users.noreply.github.com>

--- FrameGUI.java
+++ FrameGUI.java
@@ -1,6 +1,16 @@
 import java.swing.*;
 
-class PanelExample {
+public class PanelExample extends frame {
+
+private string ownerName;
+private int ownerID;
+private string ownerPassword;
+    
+private string userName;
+private int userID;
+private string userPassword;
+
+    
     public static void main(String[] args) {
         JFrame frame = new JFrame("JPanel Example");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -16,7 +26,11 @@ class PanelExample {
         frame.setVisible(true);
 
 
-        // Adding in text boxes for username and password
+        // Adding in text boxes for username and password creation
+        
+        JLabel passwordLabel = new JLabel("Username:");
+        panel.add(passwordLabel);
+        
         JTextField usernameField = new JTextField(15);
 
         panel.add(usernameField);  
@@ -26,6 +40,23 @@ class PanelExample {
 
         JPasswordField passwordField = new JPasswordField(15);
         panel.add(passwordField);
+
+        createButton();
+        
+    }
+
+    
+ // Create button
+   private void createButton()
+   {
+      button = new JButton("Create Account");
+      
+      ActionListener listener = new AddInterestListener();
+      button.addActionListener(listener);
+      
+   }
+
+    private
 
     }
 }
