608bfff6fd75e92f8a3863ac77b01b2ec9afd26e
Evans Cortez <evans.cortez23@my.stjohns.edu>
2026-04-07 10:32:18 -0400

    fixed on some issues

--- src/views/panels/MonitorPanel.java
+++ src/views/panels/MonitorPanel.java
@@ -1,5 +1,38 @@
 package views.panels;
 
-public class MonitorPanel {
-    
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+public class MonitorPanel extends JPanel {
+
+    private final JTextArea monitorArea;
+
+    public MonitorPanel() {
+        setLayout(new BorderLayout());
+
+        monitorArea = new JTextArea();
+        monitorArea.setEditable(false);
+
+        add(new JScrollPane(monitorArea), BorderLayout.CENTER);
+    }
+
+    public void appendMessage(String message) {
+        if (message == null || message.isBlank()) {
+            return;
+        }
+
+        monitorArea.append(message + System.lineSeparator());
+        monitorArea.setCaretPosition(monitorArea.getDocument().getLength());
+    }
+
+    public void setMessage(String message) {
+        monitorArea.setText(message == null ? "" : message);
+        monitorArea.setCaretPosition(monitorArea.getDocument().getLength());
+    }
+
+    public JTextArea getMonitorArea() {
+        return monitorArea;
+    }
 }
--- src/views/panels/TaskOwnerPanel.java
+++ src/views/panels/TaskOwnerPanel.java
@@ -1,5 +1,17 @@
 package views.panels;
 
-public class TaskOwnerPanel {
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import services.RequestClientService;
+
+public class TaskOwnerPanel extends JPanel {
     
 }
