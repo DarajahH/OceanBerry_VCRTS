7531da5696c0778056fdd824d66dbddfc95ffcad
EvansCortez <157630075+EvansCortez@users.noreply.github.com>
2026-04-08 14:55:05 -0400

    Update comment for clarity in VCRTSDashboard.java
    Signed-off-by: EvansCortez <157630075+EvansCortez@users.noreply.github.com>

--- src/views/VCRTSDashboard.java
+++ src/views/VCRTSDashboard.java
@@ -628,7 +628,7 @@ public class VCRTSDashboard {
             String entry = String.format("[%s] ROLE:%s | ID:%s | INFO:%s | DURATION:%d | DEADLINE:%s",
                 dtf.format(arrivalTime), role, id, info, duration, formattedDeadline);
 
-            // Send this fucker to VC Controller server over socket
+            // Send this to the VC Controller server over socket
             refreshMonitor("Connecting to VC Controller server...");
 
             Socket socket = new Socket("localhost", 9806);
