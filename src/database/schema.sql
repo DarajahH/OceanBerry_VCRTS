c1c66ff136a62ebd0094fbf9b095a8f022fde2bc
Evans Cortez <evans.cortez23@my.stjohns.edu>
2026-04-09 23:06:39 -0400

    add sql file onto the database folder.

---
+++ src/database/schema.sql
@@ -0,0 +1,13 @@
+-- Create a database for the VCRTS project for Milestone 6
+CREATE DATABASE IF NOT EXISTS vcrts_db;
+USE vcrts_db
+
+-- Table for the client jobs (ROLE: CLIENT)
+CREATE TABLE jobs (
+	id INT,
+    job_id VARCHAR(50),
+    description     VARCHAR(255),
+    duration_hours  INT,
+    arrival_time    DATETIME
+);
+    
