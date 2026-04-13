6f925a567b8e6473a7f33c28240176904b844038
Evans Cortez <evans.cortez23@my.stjohns.edu>
2026-04-13 18:47:38 -0400

    updated some java for the database folder

--- src/database/DatabaseConnection.java
+++ src/database/DatabaseConnection.java
@@ -3,6 +3,8 @@ package database;
 
 public class DatabaseConnection {
     private static final String DB_URL = "jdbc:sqlite:vcrts.db";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "";
 
     public static java.sql.Connection getConnection() throws java.sql.SQLException {
         return java.sql.DriverManager.getConnection(DB_URL);
--- src/database/DatabaseService.java
+++ src/database/DatabaseService.java
@@ -1,5 +1,12 @@
 package database;
 
 public class DatabaseService {
-    
+
+    private static final String DB_URL = "jdbc:sqlite:vcrts.db";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "";
+
+    public static java.sql.Connection getConnection() throws java.sql.SQLException {
+        return java.sql.DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+    }
 }
