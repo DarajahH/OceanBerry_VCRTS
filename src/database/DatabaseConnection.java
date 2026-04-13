92b79df2f401a10c3d78520b457f3aea023f1b14
Evans Cortez <evans.cortez23@my.stjohns.edu>
2026-04-13 18:54:53 -0400

    updated

--- src/database/DatabaseConnection.java
+++ src/database/DatabaseConnection.java
@@ -1,12 +1,10 @@
 package database;
-
-
 public class DatabaseConnection {
     private static final String DB_URL = "jdbc:sqlite:vcrts.db";
     private static final String DB_USERNAME = "root";
-    private static final String DB_PASSWORD = "";
+    private static final String DB_PASSWORD = "yourpassword";
 
     public static java.sql.Connection getConnection() throws java.sql.SQLException {
-        return java.sql.DriverManager.getConnection(DB_URL);
+        return java.sql.DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
     }
 }
