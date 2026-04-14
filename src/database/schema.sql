8af41e6e6fe281735aa280747f3d2a1dbf774ee4
Evans Cortez <evans.cortez23@my.stjohns.edu>
2026-04-13 20:28:20 -0400

    updated the clinet and all of that

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -14,21 +14,48 @@ CREATE TABLE users (
 -- Table for the client jobs (ROLE: CLIENT)
 CREATE TABLE jobs (
     job_id 			INT PRIMARY KEY,
+    client_id       INT,
     description 	VARCHAR(255) NOT NULL,
     duration_hours 	INT NOT NULL,
     arrival_time 	DATETIME,
     deadline_time 	DATETIME,
     jobStatus 		ENUM ('in_progress', 'completed', 'rejected'),
     completionTime 	INT
+    FOREIGN KEY (vehicle_id) REFERENCES vehicles(vehicle_id)
 );
 
--- Table for the vehicles owner submittor (ROLE: OWNER)
-CREATE TABLE vehicles_owner_submittor (
-    id INT PRIMARY KEY,
-    vehicle_id INT NOT NULL,
-    job_id INT NOT NULL,
+-- Table for the vehicles (ROLE: OWNER)
+CREATE TABLE vehicles (
+    vehicle_id INT PRIMARY KEY,
+    vehicle_name VARCHAR(255) NOT NULL,
+    vehicle_type VARCHAR(255) NOT NULL,
+    vehicle_status VARCHAR(255) NOT NULL,
+    vehicle_availability BOOLEAN NOT NULL,
+    FOREIGN KEY (vehicle_id) REFERENCES jobs(job_id)
+);
+
+-- Table for the logs (ROLE: ADMIN)
+CREATE TABLE logs (
+    log_id INT PRIMARY KEY,
+    log_message VARCHAR(255) NOT NULL,
+    log_timestamp DATETIME NOT NULL,
+    FOREIGN KEY (log_id) REFERENCES jobs(job_id)
+);
+
+-- Table for the notifications (ROLE: ADMIN)
+CREATE TABLE notifications (
+    notification_id INT PRIMARY KEY,
+    notification_message VARCHAR(255) NOT NULL,
+    notification_timestamp DATETIME NOT NULL,
+    FOREIGN KEY (notification_id) REFERENCES jobs(job_id)
+);
+
+-- Table for the admin decisions to request the decision from the admin (ROLE: ADMIN)
+CREATE TABLE admin_decisions (
+    request_id INT PRIMARY KEY,
+    user_id INT,
+    entry TEXT NOT NULL,
+    decision ENUM('PENDING', 'ACCEPTED', 'REJECTED') DEFAULT 'PENDING',
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (vehicle_id) REFERENCES vehicles(id),
-    FOREIGN KEY (job_id) REFERENCES jobs(id)
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
